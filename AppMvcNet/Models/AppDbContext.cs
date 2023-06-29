19b080e01c959ab7b67357fe55e2bbc6ca06e13c
unknown <hieutrinh587@gmail.com>
2023-06-29 17:14:32 +0700

    Add Contact

--- AppMvcNet/Models/AppDbContext.cs
+++ AppMvcNet/Models/AppDbContext.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AppMvcNet.Models.Contact;
+using Microsoft.EntityFrameworkCore;
 
-namespace AppMvcNet.Models
+namespace AppMvcNet
 {
+    // App.Models.AppDbContext
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
@@ -20,8 +18,18 @@ namespace AppMvcNet.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            // {
+            //     var tableName = entityType.GetTableName();
+            //     if (tableName.StartsWith("AspNet"))
+            //     {
+            //         entityType.SetTableName(tableName.Substring(6));
+            //     }
+            // }
+
         }
 
-
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
